be855ad49e1c544006ba33bdec3fe83272e97b95
parapaiva <gabrielpaiva0@gmail.com>
2023-08-28 18:27:32 -0300

    ref: fixed some tests

--- gateway/test/interface/test_service.py
+++ gateway/test/interface/test_service.py
@@ -104,7 +104,7 @@ class TestGetOrder(object):
         }
 
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
+        gateway_service.products_rpc.products_by_ids.return_value = [
             {
                 'id': 'the_odyssey',
                 'title': 'The Odyssey',
@@ -164,7 +164,8 @@ class TestGetOrder(object):
 
         # check dependencies called as expected
         assert [call(1)] == gateway_service.orders_rpc.get_order.call_args_list
-        assert [call()] == gateway_service.products_rpc.list.call_args_list
+        assert [call(['the_odyssey', 'the_enigma'])
+                ] == gateway_service.products_rpc.products_by_ids.call_args_list
 
     def test_order_not_found(self, gateway_service, web_session):
         gateway_service.orders_rpc.get_order.side_effect = (
@@ -182,7 +183,7 @@ class TestCreateOrder(object):
 
     def test_can_create_order(self, gateway_service, web_session):
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
+        gateway_service.products_rpc.products_by_ids.return_value = [
             {
                 'id': 'the_odyssey',
                 'title': 'The Odyssey',
@@ -220,7 +221,8 @@ class TestCreateOrder(object):
         )
         assert response.status_code == 200
         assert response.json() == {'id': 11}
-        assert gateway_service.products_rpc.list.call_args_list == [call()]
+        assert gateway_service.products_rpc.products_by_ids.call_args_list == [
+            call(['the_odyssey'])]
         assert gateway_service.orders_rpc.create_order.call_args_list == [
             call([
                 {'product_id': 'the_odyssey', 'quantity': 3, 'price': '41.00'}
@@ -259,7 +261,7 @@ class TestCreateOrder(object):
         self, gateway_service, web_session
     ):
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
+        gateway_service.products_rpc.products_by_ids.return_value = [
             {
                 'id': 'the_odyssey',
                 'title': 'The Odyssey',
