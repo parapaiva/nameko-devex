42eb854f9b85798d4e42c5ad7c97d1295d491ce4
parapaiva <gabrielpaiva0@gmail.com>
2023-08-28 14:44:38 -0300

    feat: service function to list products by product id

--- products/products/service.py
+++ products/products/service.py
@@ -26,6 +26,11 @@ class ProductsService:
         return schemas.Product(many=True).dump(products).data
 
     @rpc
+    def products_by_ids(self, product_ids):
+        products = self.storage.list_by_ids(product_ids)
+        return schemas.Product(many=True).dump(products).data
+
+    @rpc
     def create(self, product):
         product = schemas.Product(strict=True).load(product).data
         self.storage.create(product)
